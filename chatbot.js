b49d85378818847852eb20d8f9804838a97513be
lmxs1237 <43193537+lmxs1237@users.noreply.github.com>
2018-10-17 20:25:09 -0400

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $('#sendbtn').bind("click",sendmsg);
+    //bind "send" action with button
+    function sendmsg(){
+            var txt=$("#quest").val();            
+            //Get input message
+            if (txt.length===0){
+                alert("The sending messege can not be empty!");
+            }
+            else{
+
+            var option=$("<option></option>");
+            //Set a new option
+            var len=txt.length;
+            option.css("width",len*8+"px");
+            option.css("marginLeft",340-len*8-50+"px");
+            //Setting option form
+            option.html(txt);
+            //Get the continer of the option
+            $("#clicontent").append(option);
+            //Add option on the page
+
+            $(".sendtext").val("");
+            //Clear text
+            
+            var apigClient = apigClientFactory.newClient({
+                accessKey: 'AKIAJPNA77FOQV73WOVA',
+                // secretKey: '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',
+                // sessionToken: 'SESSION_TOKEN', //OPTIONAL: If you are using temporary credentials you must include the session token
+                // region: 'us-east-2' // OPTIONAL: The region where the API is deployed, by default this parameter is set to us-east-1
+                
+            });
+            var body={
+                    "question":txt
+      
+             };
+
+             var params={}
+             var additionalParams={}
+            apigClient.chatbotPost(params,body,additionalParams)
+                .then(function(data){
+                    
+                    data=data['data']
+                    var option=$("<option></option>");
+                    
+                    var len=data.length;
+                    option.css("width",len*6.8+"px");
+                    option.css("marginleft",5+"px");
+                    //option.css("background",rgba(220,220,220) );
+                    //Setting option form
+                    option.html(data);
+                    //Get the continer of the option
+                    $("#clicontent").append(option);
+              }).catch(function(data){
+                    console.log(data)
+                });
+
+            }        
+    }    
+
+    $(document).keydown(function(event){
+           var text = $("#quest").val();
+           if(event.keyCode == 13){
+                           sendmsg();
+                  }
+            });
+});
---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    /*cognito: {
+        userPoolId: 'us-east-2_pbPc9DZup', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6rtl34drnu5ld09dalijgqfpgt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },*/
+    api: {
+        invokeUrl: 'https://ghezl5xo12.execute-api.us-east-2.amazonaws.com/beta' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
